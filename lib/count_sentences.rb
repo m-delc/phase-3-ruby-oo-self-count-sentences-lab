f7d2b5f3e0e3701f5f969ab4e9a5925717760848
Michael Delcambre <delcambrem@gmail.com>
2022-01-12 19:48:07 -0600

    lab complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,21 +14,8 @@ class String
     self.end_with?("!")
   end
 
-    # this works for all but the last
-  # def count_sentences
-  #   x = self.split(' ')
-  #   y = x.count
-  #   # binding.pry
-  # end
-
-  # def count_sentences
-  #   x = self.split(/[\s]/)
-  #   y = x.count
-  #   binding.pry
-  # end 
   def count_sentences
     x = self.split(/[?!.] /)
     y = x.count
-    # binding.pry
   end
 end
