5054722e2e2c4c11b1cbb52a9e2690e39bea1536
Michael Delcambre <delcambrem@gmail.com>
2022-01-12 19:47:05 -0600

    lab complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+    # this works for all but the last
+  # def count_sentences
+  #   x = self.split(' ')
+  #   y = x.count
+  #   # binding.pry
+  # end
 
+  # def count_sentences
+  #   x = self.split(/[\s]/)
+  #   y = x.count
+  #   binding.pry
+  # end 
+  def count_sentences
+    x = self.split(/[?!.] /)
+    y = x.count
+    # binding.pry
   end
 end
